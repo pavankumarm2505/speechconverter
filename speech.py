40a9a1f5bdc5b673632497bec19981bd4374a3a0
Pavan Kumar Mathamsetty <pavankumarm2505@gmail.com>
2023-08-31 15:00:16 -0400

    first commit

---
+++ speech.py
@@ -0,0 +1,23 @@
+from gtts import gTTS
+import os
+from tkinter import *
+
+def text_to_speech():
+    text = entry.get()
+    language = 'en'
+    output = gTTS(text = text, lang = language, slow = False)
+    output.save("output.mp3")
+
+    os.system("start output.mp3")
+root = Tk()
+canvas = Canvas(root,width = 400, height= 400)
+canvas.pack()
+
+entry = Entry(root)
+canvas.create_window(200,180,window=entry
+)
+
+button = Button(text = "Start",command= text_to_speech)
+canvas.create_window(200,230,window=button)
+
+root.mainloop()
